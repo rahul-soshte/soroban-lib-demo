7a16e426e9c618e7bbb72405e1ebda905bc8a00e
root <rahul.soshte4771@gmail.com>
2024-02-06 10:24:51 +0000

    get health working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[allow(warnings)]
 use soroban_client::server::Options;
 use soroban_client::{server::Server, keypair::Keypair};
 use soroban_client::keypair::KeypairBehavior;
@@ -6,39 +7,11 @@ use soroban_client::keypair::KeypairBehavior;
 async fn main() {
     let source_secret_key = "SCCTADNI4B4FEFELEYEYSDUNQXVTXHRAOEXWWJWHJ57EO3VHGXJFL3TC";
     let source_keypair = Keypair::from_secret(source_secret_key).expect("Invalid secret key");
-    let source_public_key = source_keypair.public_key();
-    let contract_id = "CCJYKPKPQADXVZVGNJIDIUFNBMV6FOKCFZZZMA2FCEZOMDIQA5BBPPCN";
-    let server = Server::new("https://rpc-futurenet.stellar.org:443", Options{ allow_http: Some(true), timeout: Some(1000), headers: None });
-    let account = server.get_account(&source_public_key).await.unwrap();
-// let fee = 100;
+    let _source_public_key = source_keypair.public_key();
+    let _source_public_key = "GDWH3P3MNTCMOY42CA7RVEACUUAUPZ73XDYKPYUL3TWOFRF37FD6OVM6";
 
-// let contract = Contracts::new(contract_id);
-
-// let mut transaction2 = TransactionBuilder::new(account, "Test SDF Future Network ; October 2022")
-//     .add_operation(contract.call("hello"))
-//     .build();
-
-// let transaction3 = server.prepare_transaction(transaction2, Some("Test SDF Future Network ; October 2022")).await.unwrap();
-// println!("{:?}", transaction3);
-//     transaction.sign(&source_keypair);
-
-//     // Let's see the XDR (encoded in base64) of the transaction we just built
-//     println!("XDR: {}", transaction.to_envelope().to_xdr_base64().expect("Failed to convert to XDR"));
-
-//     match server.send_transaction(transaction) {
-//         Ok(mut response) => {
-//             println!("Sent! Transaction ID: {}", response.id);
-
-//             while response.status == "pending" {
-//                 response = server.get_transaction_status(&response.id).expect("Failed to get transaction status");
-//                 sleep(Duration::from_secs(1));
-//             }
-
-//             println!("Transaction status: {}", response.status);
-//             println!("{:?}", response);
-//         },
-//         Err(e) => {
-//             eprintln!("An error has occured: {:?}", e);
-//         }
-//     }
+    let _contract_id = "CCJYKPKPQADXVZVGNJIDIUFNBMV6FOKCFZZZMA2FCEZOMDIQA5BBPPCN";
+    let _server = Server::new("https://rpc-futurenet.stellar.org:443/", Options{ allow_http: Some(true), timeout: Some(1000), headers: None });
+    let health = _server.get_health().await.unwrap();
+    println!("{:?}", health);
 }
